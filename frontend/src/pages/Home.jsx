849f73af035dd43b8e10156ede1b766a7ece3fc3
Melkamzer <melkamzerantish@gmail.com>
2025-09-03 12:57:06 +0300

    I added some file....

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,9 +6,21 @@ function Home(){
         {id:3, title:"The Lord of the Rings", release_date:"2023-01-01"},
     ]
     
+    const handle=()=>{
+    }
+
     return(
-        <div>
-            {movies.map(movie=> <MovieCard movie={movie} key={movie.name}/>)}
+
+        <div className="home">
+            
+            <form onSubmit={handlesearch} className="search-form">
+                <input type="text" placeholder="Search for a movie..." className="Search"></input>
+            </form>
+
+            <div className="movie-grid">
+                {movies.map(movie=> <MovieCard movie={movie} key={movie.name}/>)}
+            </div>
+
         </div>
     )
 }
